99cddb031451cf63d637fda0784ecd3c1d6626b6
andreadilisio <andrea.dilisio@onebip.com>
2019-12-09 21:08:26 +0100

    integration test isolated gained with junit5 + surefire config

--- src/test/java/org/acme/shakesperean_pokedex/IntegrationTest.java
+++ src/test/java/org/acme/shakesperean_pokedex/IntegrationTest.java
@@ -3,12 +3,14 @@ package org.acme.shakesperean_pokedex;
 import io.quarkus.test.junit.QuarkusTest;
 import org.acme.shakesperean_pokedex.dto.PokedexResult;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
+@Tag("IntegrationTest")
 public class IntegrationTest {
 
     private static final String A_POKEMON_NAME = "charizard";
