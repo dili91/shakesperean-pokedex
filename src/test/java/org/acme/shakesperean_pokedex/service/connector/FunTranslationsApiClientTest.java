3d682b4227ff66685d35018c0ccf2be94d64ffd8
andreadilisio <andrea.dilisio@onebip.com>
2019-12-09 15:29:39 +0100

    api connectors tests not depending on quarkus application context

--- src/test/java/org/acme/shakesperean_pokedex/service/connector/FunTranslationsApiClientTest.java
+++ src/test/java/org/acme/shakesperean_pokedex/service/connector/FunTranslationsApiClientTest.java
@@ -1,31 +1,36 @@
 package org.acme.shakesperean_pokedex.service.connector;
 
-import io.quarkus.test.junit.QuarkusTest;
 import org.acme.shakesperean_pokedex.dto.fun_translations.Translation;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import javax.inject.Inject;
+import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@QuarkusTest
+@DisplayName("Test fot FunTranslations API connector")
 class FunTranslationsApiClientTest {
-    // todo optional: make this quarkus independent
     // todo optional: stub external api
 
+    private static final String FUN_TRANSLATIONS_API_ENDPOINT = "https://api.funtranslations.com/translate/shakespeare.json";
     private static final String A_TEXT_IN_MODERN_ENGLISH_LANGUAGE = "You gave Mr. Tim a hearty meal, but unfortunately what he ate made him die.";
     private static final String A_TEXT_IN_SHAKESPEAREAN_LANGUAGE = "Thou did giveth Mr. Tim a hearty meal,  but unfortunately what he englut did maketh him kicketh the bucket.";
 
-    @Inject
-    @RestClient
-    FunTranslationsApiClient funTranslationsApiClient;
+    private static FunTranslationsApiClient testClient;
+
+    @BeforeAll
+    static void setup() {
+        testClient = RestClientBuilder.newBuilder()
+                .baseUri(URI.create(FUN_TRANSLATIONS_API_ENDPOINT))
+                .build(FunTranslationsApiClient.class);
+    }
 
     @Test
     @DisplayName("should get a text translated in Shakespeare's language")
-    public void shouldGetAShakespeareanTranslation(){
-        Translation translation = funTranslationsApiClient.translate(A_TEXT_IN_MODERN_ENGLISH_LANGUAGE);
+    public void shouldGetAShakespeareanTranslation() {
+        Translation translation = testClient.translate(A_TEXT_IN_MODERN_ENGLISH_LANGUAGE);
 
         assertEquals(A_TEXT_IN_MODERN_ENGLISH_LANGUAGE, translation.getContents().getText());
         assertEquals(A_TEXT_IN_SHAKESPEAREAN_LANGUAGE, translation.getContents().getTranslated());
--- src/test/java/org/acme/shakesperean_pokedex/service/connector/PokeApiClientTest.java
+++ src/test/java/org/acme/shakesperean_pokedex/service/connector/PokeApiClientTest.java
@@ -1,35 +1,43 @@
 package org.acme.shakesperean_pokedex.service.connector;
 
-import io.quarkus.test.junit.QuarkusTest;
 import org.acme.shakesperean_pokedex.dto.poke_api.PokemonSpecies;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
+import java.net.URI;
 
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@QuarkusTest
+@DisplayName("Test fot PokeApi API connector")
 public class PokeApiClientTest {
 
-    // todo optional: make this quarkus independent
     // todo optional: stub external api
 
+    private static final String POKE_API_ENDPOINT = "https://pokeapi.co";
     private static final String A_POKEMON_NAME = "charizard";
     private static final String A_POKEMON_COLOR = "red";
     private static final String A_POKEMON_DESCRIPTION = "Charizard flies around the sky in search of powerful opponents.\nIt breathes fire of such great heat that it melts anything.\nHowever, it never turns its fiery breath on any opponent\nweaker than itself.";
     private static final String A_DEFAULT_LANGUAGE = "en";
     private static final String A_DEFAULT_VERSION = "alpha-sapphire";
 
-    @Inject
-    @RestClient
-    PokeApiClient pokeApiClient;
+    private static PokeApiClient testClient;
+
+    @BeforeAll
+    static void setup() {
+        testClient = RestClientBuilder.newBuilder()
+                .baseUri(URI.create(POKE_API_ENDPOINT))
+                .build(PokeApiClient.class);
+    }
 
     @Test
     @DisplayName("Should get a pokemon species")
     public void shouldGetAPokemonDescription() {
-        PokemonSpecies species = pokeApiClient.getPokemonSpecies(A_POKEMON_NAME);
+        PokemonSpecies species = testClient.getPokemonSpecies(A_POKEMON_NAME);
 
         assertEquals(A_POKEMON_NAME, species.getName());
         assertEquals(A_POKEMON_COLOR, species.getColor().getName());
@@ -38,8 +46,17 @@ public class PokeApiClientTest {
                         .filter(entry -> entry.getLanguage().getName().equalsIgnoreCase(A_DEFAULT_LANGUAGE))
                         .filter(entry -> entry.getVersion().getName().equalsIgnoreCase(A_DEFAULT_VERSION))
                         .findFirst()
-                        .orElseThrow()
+                        .get()
                         .getFlavorText()
         );
+    }
+
+    @Test
+    @DisplayName("Should get an exception if pokemon species is not found")
+    public void shouldGetAWebServiceException() {
+        WebApplicationException exception = assertThrows(WebApplicationException.class,
+                () -> testClient.getPokemonSpecies("a-non-existing-pokemon-name"));
+
+        assertEquals(NOT_FOUND, exception.getResponse().getStatusInfo().toEnum());
     }
 }
