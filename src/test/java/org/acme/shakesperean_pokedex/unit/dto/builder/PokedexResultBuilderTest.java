59feb4ce9a24183b67515352b8205b9e29e6b56b
andreadilisio <andrea.dilisio@onebip.com>
2019-12-10 01:57:01 +0100

    refactor

---
+++ src/test/java/org/acme/shakesperean_pokedex/unit/dto/builder/PokedexResultBuilderTest.java
@@ -0,0 +1,5 @@
+package org.acme.shakesperean_pokedex.unit.dto.builder;
+
+public class PokedexResultBuilderTest {
+    //todo
+}
--- src/test/java/org/acme/shakesperean_pokedex/unit/service/dto/builder/PokedexResultBuilderTest.java
+++
@@ -1,5 +0,0 @@
-package org.acme.shakesperean_pokedex.unit.service.dto.builder;
-
-public class PokedexResultBuilderTest {
-    //todo
-}
