be335e69779957dd714786f1c8fb90e385199e7f
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2020-04-01 15:13:18 +0200

    added todo in test

--- src/test/java/org/acme/shakesperean_pokedex/util/RemoteApiTest.java
+++ src/test/java/org/acme/shakesperean_pokedex/util/RemoteApiTest.java
@@ -3,6 +3,9 @@ package org.acme.shakesperean_pokedex.util;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+/**
+ * todo: fix dependency issue that make all integration tests fail after quarkus upgrade to 1.3.1.Final
+ */
 @ExtendWith(MockServerExtension.class)
 public abstract class RemoteApiTest {
 
